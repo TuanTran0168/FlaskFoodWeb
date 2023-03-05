503e3343c5790ea6fd164935b1bff52403c01b6d
TuanTran <tamthy12345@gmail.com>
2023-03-05 09:44:02 +0700

    source code

---
+++ saleappv2/saleapp/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+from flask_login import LoginManager
+from flask_babelex import Babel
+import cloudinary
+
+app = Flask(__name__)
+app.secret_key = '689567gh$^^&*#%^&*^&%^*DFGH^&*&*^*'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/foodappdb?charset=utf8mb4' % quote('Admin@123')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['CART_KEY'] = 'cart'
+
+cloudinary.config(cloud_name='dhwuwy0to', api_key='569153767496484', api_secret='ghXq0iY8RhWbqBcJaide7W-34RY')
+
+db = SQLAlchemy(app=app)
+
+login = LoginManager(app=app)
+
+babel = Babel(app=app)
+
+
+@babel.localeselector
+def load_locale():
+    return 'vi'
